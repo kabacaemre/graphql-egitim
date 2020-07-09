ab8774b4522bf935ed729aab32b01d0eee27fd4d
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-09 04:21:50 +0300

    dotenv && mongoose && mongodb connect
    dotenv && mongoose && mongodb connect

--- easysnap/app.js
+++ easysnap/app.js
@@ -2,16 +2,27 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { loadFile } = require('graphql-import-files');
 
-const resolvers = require('./graphql/resolvers/index');
+//Express
+const app = express();
 
+// Environment Variables
+const dotenv = require('dotenv');
+dotenv.config({path: "./config/env/config.env"});
+
+// MongoDB Connection
+const connectDatabase = require('./helpers/db.js');
+connectDatabase();
+
+//Resolvers
+const resolvers = require('./graphql/resolvers');
+
+//ApolloServer
 const server = new ApolloServer({
 	typeDefs: loadFile('./graphql/types/schema.graphql'),
 	resolvers
 });
-
-const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ port: 4001 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`)
+  console.log(`Server ready at http://localhost:4001${server.graphqlPath}`)
 )
