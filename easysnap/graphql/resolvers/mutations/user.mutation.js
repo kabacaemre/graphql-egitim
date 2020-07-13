ff3020a9a65446bf1dfea77a6728b1076b19dbc8
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-13 21:38:30 +0300

    createUser generate token

--- easysnap/graphql/resolvers/mutations/user.mutation.js
+++ easysnap/graphql/resolvers/mutations/user.mutation.js
@@ -4,25 +4,28 @@ const token = require('../../../helpers/token');
 module.exports = {
 	createUser: async (parent, { data: { username, password } }, { User }) => {
 		const user = await User.findOne({ username });
-
 		if (user) {
 		  throw new Error('User already exists.');
 		}
 
-		return await new User({
+		const newUser = await new User({
 			username,
 			password
 		}).save();
+
+		return { token: token.generate(newUser, '1h') }
 	},
 	signIn: async (parent, { data: { username, password } }, { User }) => {
 		const user = await User.findOne({ username });
 		if (!user) {
 			throw new Error("User does not exists!");
 		}
+
 		const validPassword = await bcrypt.compare(password, user.password);
 		if (!validPassword) {
 			throw new Error("Wrong password!");
 		}
+		
 		return { token: token.generate(user, '1h') }
 	}
 };
