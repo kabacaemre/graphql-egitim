35062edd6b1f139aaa2dc8f66be66bec778a8294
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-13 17:55:59 +0300

    user snaps query

--- easysnap/graphql/resolvers/index.js
+++ easysnap/graphql/resolvers/index.js
@@ -1,7 +1,8 @@
 const Query = require('./queries/Query');
 const Snap = require('./queries/Snap');
+const User = require('./queries/User');
 const Mutation = require('./mutations');
 
 module.exports = {
-	Query, Snap, Mutation
+	Query, Snap, User, Mutation
 };
