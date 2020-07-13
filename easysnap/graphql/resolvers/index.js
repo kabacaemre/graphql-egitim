4ccb84ddf6f1356f7d4979bb5411a73e5d14b857
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-14 02:53:53 +0300

    ui

--- easysnap/graphql/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-const Query = require('./queries/Query');
-const Snap = require('./queries/Snap');
-const User = require('./queries/User');
-const Mutation = require('./mutations');
-
-module.exports = {
-	Query, Snap, User, Mutation
-};
