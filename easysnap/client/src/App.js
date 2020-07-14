01fa55ab66209d8c8b56e4d6575c49010cfb34ee
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-15 02:43:18 +0300

    ui

---
+++ easysnap/client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+    return (
+        <div className="App">
+            <div class="container">
+                <Header />
+                <Home />
+            </div>
+        </div>
+    );
+}
+
+export default App;
