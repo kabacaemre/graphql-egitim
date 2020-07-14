495361d7e43566edb2d640943392e13502a2dd14
Emre Kabaca <emre_kabaca123@hotmail.com>
2020-07-14 03:37:41 +0300

    Revert "ui"
    This reverts commit 4ccb84ddf6f1356f7d4979bb5411a73e5d14b857.

---
+++ easysnap/graphql/resolvers/index.js
@@ -0,0 +1,8 @@
+const Query = require('./queries/Query');
+const Snap = require('./queries/Snap');
+const User = require('./queries/User');
+const Mutation = require('./mutations');
+
+module.exports = {
+	Query, Snap, User, Mutation
+};
